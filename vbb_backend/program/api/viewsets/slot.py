730b855c5fed19af20f4908a43592b5c89cc4aa8
Dustin Hamano <43916076+dhamano@users.noreply.github.com>
2021-05-11 16:02:46 -0700

    got schedule_start filter working. WIP: schedule_end, date filtering

--- vbb_backend/program/api/viewsets/slot.py
+++ vbb_backend/program/api/viewsets/slot.py
@@ -1,3 +1,4 @@
+import datetime, logging
 from drf_yasg.utils import swagger_auto_schema
 from dry_rest_permissions.generics import DRYPermissions
 from rest_framework import status
@@ -8,6 +9,7 @@ from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from django_filters import rest_framework as filters
 
 from vbb_backend.program.api.serializers.slot import (
     MinimalSlotSerializer,
@@ -16,10 +18,12 @@ from vbb_backend.program.api.serializers.slot import (
 from vbb_backend.program.models import Computer, Program, Slot
 from vbb_backend.users.models import UserTypeEnum
 
+logger = logging.getLogger('django.db.backends')
+
 
 class SlotViewSet(ModelViewSet):
     queryset = Slot.objects.all()
-    permission_classes = [IsAuthenticated, DRYPermissions]
+    permission_classes = [IsAuthenticated]
     serializer_class = SlotSerializer
     lookup_field = "external_id"
 
@@ -53,3 +57,32 @@ class ReadOnlySlotViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = MinimalSlotSerializer
     lookup_field = "external_id"
+    
+    # def get_slots(self, for_schedule_start, for_schedule_end):
+    #     print(f'\n\n\n\n{for_schedule_start}\n\n\n\n')
+    #     return Slot.objects.filter(schedule_start__gte=for_schedule_start)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        start_day_of_week = self.request.query_params.get("start_day_of_week") or 1
+        start_hour = self.request.query_params.get("start_hour") or 0
+        start_minute = self.request.query_params.get("start_minute") or 0
+        for_schedule_start = Slot.DEAFULT_INIT_DATE + datetime.timedelta(
+            days=int(start_day_of_week), hours=int(start_hour), minutes=int(start_minute)
+        )
+        
+        end_day_of_week = self.request.query_params.get("end_day_of_the_week") or 7
+        end_hour = self.request.query_params.get("end_hour") or 23
+        end_minute = self.request.query_params.get("end_minute") or 59
+        for_schedule_end = Slot.DEAFULT_INIT_DATE + datetime.timedelta(days=int(end_day_of_week), hours=int(end_hour), minutes=int(end_minute))
+        
+        return queryset.filter(schedule_start__gte=for_schedule_start)
+        # slot = self.get_slots(for_schedule_start, for_schedule_end)
+        # slot_set = slot
+
+        # if slot is None:
+        #     return self.queryset.none()
+
+        # return slot
+
+
