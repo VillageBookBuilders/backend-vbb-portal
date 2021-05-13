ade9c6268707dce239287610f5dbf21e98349286
Dustin Hamano <43916076+dhamano@users.noreply.github.com>
2021-05-13 16:22:52 -0700

    added filtering by language, schedule start and end, computer_id, max_students, is_mentor_assigned, is_student_assigned

--- vbb_backend/program/api/viewsets/slot.py
+++ vbb_backend/program/api/viewsets/slot.py
@@ -52,41 +52,56 @@ class SlotViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(computer=self.get_computer())
 
-
 class ReadOnlySlotViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Slot.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = MinimalSlotSerializer
     lookup_field = "external_id"
-    
-    # def get_slots(self, for_schedule_start, for_schedule_end):
-    #     print(f'\n\n\n\n{for_schedule_start}\n\n\n\n')
-    #     return Slot.objects.filter(schedule_start__gte=for_schedule_start)
 
     def get_queryset(self):
         queryset = self.queryset
-        start_day_of_week = self.request.query_params.get("start_day_of_week") or 1
+        start_day_of_week = self.request.query_params.get("start_day_of_week") or 0
         start_hour = self.request.query_params.get("start_hour") or 0
         start_minute = self.request.query_params.get("start_minute") or 0
         for_schedule_start = Slot.DEAFULT_INIT_DATE + datetime.timedelta(
             days=int(start_day_of_week), hours=int(start_hour), minutes=int(start_minute)
         )
         
-        end_day_of_week = self.request.query_params.get("end_day_of_week") or 7
+        end_day_of_week = self.request.query_params.get("end_day_of_week") or 6
         end_hour = self.request.query_params.get("end_hour") or 23
         end_minute = self.request.query_params.get("end_minute") or 59
         for_schedule_end = Slot.DEAFULT_INIT_DATE + datetime.timedelta(
             days=int(end_day_of_week), hours=int(end_hour), minutes=int(end_minute)
         )
+
+        computer_id = self.request.query_params.get("computer_id")
+        max_students = self.request.query_params.get("max_students")
+        language = self.request.query_params.get("language")
+        is_mentor_assigned = self.request.query_params.get("is_mentor_assigned")
+        is_student_assigned = self.request.query_params.get("is_student_assigned")
         
-        return queryset.filter(Q(schedule_start__gte=for_schedule_start), Q(schedule_end__lte=for_schedule_end))
-        # return queryset.filter(schedule_end__lte=for_schedule_end)
-        # slot = self.get_slots(for_schedule_start, for_schedule_end)
-        # slot_set = slot
+        # return queryset.filter(Q(schedule_start__gte=for_schedule_start), Q(schedule_end__lte=for_schedule_end))
+        # matching_slots = F({'schedule_start': for_schedule_start, 'schedule_end': for_schedule_end}, queryset=queryset)
+        matching_slots = F({
+            'schedule_start': for_schedule_start,
+            'schedule_end': for_schedule_end,
+            "computer_id": computer_id,
+            "max_students": max_students,
+            "is_mentor_assigned": is_mentor_assigned,
+            "is_student_assigned": is_student_assigned,
+            "language": language,
+            }, queryset=queryset)
+        return matching_slots.qs
 
-        # if slot is None:
-        #     return self.queryset.none()
+class F(filters.FilterSet):
+    language = filters.CharFilter(field_name="language")
+    schedule_start = filters.IsoDateTimeFilter(field_name="schedule_start", lookup_expr="gte")
+    schedule_end = filters.IsoDateTimeFilter(field_name="schedule_end", lookup_expr="lte")
+    computer_id = filters.UUIDFilter(field_name="computer_id__external_id")
+    max_students = filters.NumberFilter(field_name="max_students", lookup_expr="lte")
+    is_mentor_assigned = filters.BooleanFilter(field_name="is_mentor_assigned")
+    is_student_assigned = filters.BooleanFilter(field_name="is_student_assigned")
 
-        # return slot
-
-
+    class Meta:
+        model = Slot
+        fields = ("__all__")
