0e0e314da806351ad0e31256bdb90edef2d41248
Dustin Hamano <43916076+dhamano@users.noreply.github.com>
2021-05-21 15:10:13 -0700

    filtering updated to filterset_class

--- vbb_backend/program/api/viewsets/slot.py
+++ vbb_backend/program/api/viewsets/slot.py
@@ -3,13 +3,14 @@ from drf_yasg.utils import swagger_auto_schema
 from dry_rest_permissions.generics import DRYPermissions
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from django_filters import rest_framework as filters
+from django.db.models import Q
 
 from vbb_backend.program.api.serializers.slot import (
     MinimalSlotSerializer,
@@ -49,55 +50,57 @@ class SlotViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(computer=self.get_computer())
 
+
+class SlotFilterSet(filters.FilterSet):
+    computer_id = filters.UUIDFilter(field_name="computer_id__external_id")
+    max_students = filters.NumberFilter(field_name="max_students", lookup_expr="lte")
+    language = filters.CharFilter(field_name="language")
+    is_mentor_assigned = filters.BooleanFilter(field_name="is_mentor_assigned")
+    is_student_assigned = filters.BooleanFilter(field_name="is_student_assigned")
+    
+
 class ReadOnlySlotViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Slot.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = MinimalSlotSerializer
     lookup_field = "external_id"
+    filterset_class = SlotFilterSet
 
     def get_queryset(self):
         queryset = self.queryset
-        start_day_of_week = self.request.query_params.get("start_day_of_week") or 0
-        start_hour = self.request.query_params.get("start_hour") or 0
-        start_minute = self.request.query_params.get("start_minute") or 0
+        start_day_of_week = self.request.GET.get("start_day_of_week", 0)
+        start_hour = self.request.GET.get("start_hour", 0)
+        start_minute = self.request.GET.get("start_minute", 0)
         schedule_start = Slot.DEAFULT_INIT_DATE + datetime.timedelta(
             days=int(start_day_of_week), hours=int(start_hour), minutes=int(start_minute)
         )
         
-        end_day_of_week = self.request.query_params.get("end_day_of_week") or 6
-        end_hour = self.request.query_params.get("end_hour") or 23
-        end_minute = self.request.query_params.get("end_minute") or 59
+        end_day_of_week = self.request.GET.get("end_day_of_week", 6)
+        end_hour = self.request.GET.get("end_hour", 23)
+        end_minute = self.request.GET.get("end_minute", 59)
         schedule_end = Slot.DEAFULT_INIT_DATE + datetime.timedelta(
             days=int(end_day_of_week), hours=int(end_hour), minutes=int(end_minute)
         )
 
-        computer_id = self.request.query_params.get("computer_id")
-        max_students = self.request.query_params.get("max_students")
-        language = self.request.query_params.get("language")
-        is_mentor_assigned = self.request.query_params.get("is_mentor_assigned")
-        is_student_assigned = self.request.query_params.get("is_student_assigned")
+        if(int(start_day_of_week) < 0 or int(start_day_of_week) > 6):
+            raise ValidationError({"schedule": "Start day of week must be between 0 and 6"})
 
-        matching_slots = F({
-            "schedule_start": schedule_start,
-            "schedule_end": schedule_end,
-            "computer_id": computer_id,
-            "max_students": max_students,
-            "is_mentor_assigned": is_mentor_assigned,
-            "is_student_assigned": is_student_assigned,
-            "language": language,
-            }, queryset=queryset)
+        if(int(end_day_of_week) < 0 or int(end_day_of_week) > 6):
+            raise ValidationError({"schedule": "End day of week must be between 0 and 6"})
 
-        return matching_slots.qs
+        if(int(start_hour) < 0 or int(start_hour) > 23):
+            raise ValidationError({"schedule": "Start hour must be between 0 and 23"})
 
-class F(filters.FilterSet):
-    language = filters.CharFilter(field_name="language")
-    schedule_start = filters.IsoDateTimeFilter(field_name="schedule_start", lookup_expr="gte")
-    schedule_end = filters.IsoDateTimeFilter(field_name="schedule_end", lookup_expr="lte")
-    computer_id = filters.UUIDFilter(field_name="computer_id__external_id")
-    max_students = filters.NumberFilter(field_name="max_students", lookup_expr="lte")
-    is_mentor_assigned = filters.BooleanFilter(field_name="is_mentor_assigned")
-    is_student_assigned = filters.BooleanFilter(field_name="is_student_assigned")
+        if(int(end_hour) < 0 or int(end_hour) > 23):
+            raise ValidationError({"schedule": "End hour must be between 0 and 23"})
 
-    class Meta:
-        model = Slot
-        fields = ("__all__")
+        if(int(start_minute) < 0 or int(start_minute) > 59):
+            raise ValidationError({"schedule": "Start minute must be between 0 and 59"})
+
+        if(int(end_minute) < 0 or int(end_minute) > 59):
+            raise ValidationError({"schedule": "End minute must be between 0 and 59"})
+        
+        if(schedule_start > schedule_end):
+            raise ValidationError({"schedule": "Start date cannot be after end date"})
+
+        return queryset.filter(Q(schedule_start__gte=schedule_start), Q(schedule_end__lte=schedule_end))
