a09aff42a62a5a7d633e50fcdc68f89e3fd56896
sclausen1 <stevenclausen1@gmail.com>
2021-05-21 21:37:18 -0400

    added SubFactories

--- vbb_backend/users/tests/factories.py
+++ vbb_backend/users/tests/factories.py
@@ -1,4 +1,5 @@
 import factory
+from factory.declarations import SubFactory
 from faker import Faker
 import factory
 from vbb_backend.users.models import *
@@ -18,35 +19,42 @@ class UserFactory(factory.django.DjangoModelFactory):
 class MentorFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Mentor
+    user = factory.SubFactory(UserFactory)
 
 class ExecutiveFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Executive
+    user = factory.SubFactory(UserFactory)
 
 
 class HeadmasterFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Headmaster
+    user = factory.SubFactory(UserFactory)
 
 
 class ParentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Parent
+    user = factory.SubFactory(UserFactory)
 
 
 class StudentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Student
-
+    user = factory.SubFactory(UserFactory)
 
 class TeacherFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Teacher
+    user = factory.SubFactory(UserFactory)
 
 class ProgramManagerFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = ProgramManager
+    user = factory.SubFactory(UserFactory)
 
 class ProgramDirectorFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = ProgramDirector
+    user = factory.SubFactory(UserFactory)
