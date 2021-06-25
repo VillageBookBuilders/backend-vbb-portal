eee77a38aaef6cfca72f28b8586dc5648cdd05d0
dhamano <43916076+dhamano@users.noreply.github.com>
2021-06-25 10:28:51 -0700

    removed notes

--- vbb_backend/session/tasks.py
+++ vbb_backend/session/tasks.py
@@ -5,26 +5,6 @@ from celery.schedules import crontab
 from vbb_backend.session.models import Session
 from vbb_backend.program.models import Slot
 
-    # don't create a session without a slot
-    # loook through slots taht have been passed
-    # ex. monday 7:45 look for all these slots created for this day
-    # create session for next week
-    # get list of all the slots before current time and check if there is a session instance for next week
-    # need to create slot for next week
-    # is there a session in the future connected to that slot
-    #
-    # what's the criteria for the list of slots we're iterating over
-    # have sessions that happen once a month
-    # cancel 2 weeks out or reschedule for 3 weeks out? (create another model for outages then check against that)
-    # cancellation is a future feature
-    # if mentor cannot meet mentee create materials and send to mentee
-    # allow mentors to say 2wks from now...
-    # when we save 2wks data, for that slot not available
-    # build out MVP first
-    #
-    # every time we save a slot create a session through a task
-    # every 2 hrs check every slot
-
 def get_current_time():
     now = datetime.datetime.now()
     time_now = Slot.get_slot_time(
