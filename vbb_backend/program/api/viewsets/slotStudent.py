c282fc7509943bbc246e32fcc8ccb37342d81936
dhamano <43916076+dhamano@users.noreply.github.com>
2021-07-28 12:32:44 -0700

    added get_student to perform_create with uuid check

--- vbb_backend/program/api/viewsets/slotStudent.py
+++ vbb_backend/program/api/viewsets/slotStudent.py
@@ -1,16 +1,17 @@
-from rest_framework.exceptions import PermissionDenied
+from uuid import UUID
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from vbb_backend.program.api.serializers.slotStudent import StudentSlotSerializer
 from vbb_backend.program.models import Program, Slot, StudentSlotAssociation
-from vbb_backend.users.models import UserTypeEnum
+from vbb_backend.users.models import UserTypeEnum, Student
 
 
 class StudentSlotViewSet(ModelViewSet):
     queryset = StudentSlotAssociation.objects.all()
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated]
     serializer_class = StudentSlotSerializer
     lookup_field = "external_id"
 
@@ -27,10 +28,28 @@ class StudentSlotViewSet(ModelViewSet):
             raise PermissionDenied()
         return queryset
 
+    def check_if_uuid(self, uuid_to_test):
+        try:
+            uuid_obj = UUID(uuid_to_test)
+        except ValueError:
+            return False
+        return str(uuid_obj) == uuid_to_test
+
     def get_slot(self):
         return get_object_or_404(
-            self.get_queryset(), external_id=self.kwargs.get("slot_external_id")
+            Slot, external_id=self.kwargs.get("slot_external_id")
+        )
+
+    def get_student(self):
+        return get_object_or_404(
+            Student, external_id=self.request.data.get("student")
         )
 
     def perform_create(self, serializer):
-        serializer.save(slot=self.get_slot())
+        student_id = self.request.data.get("student")
+        if not student_id:
+            raise ValidationError({"message": "mentor id required"})
+        if not self.check_if_uuid(student_id):
+            raise ValidationError({"message": "mentor id must be valid UUID"})
+
+        serializer.save(slot=self.get_slot(), student=self.get_student())
